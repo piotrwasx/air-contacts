ae9db6711817375c4edb079b96c01443d19b1487
piotrwas <ns.piotr@gmail.com>
2022-09-28 12:23:03 +0200

    Initial Commit

---
+++ AirContacts/AirContactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirContactsApp.swift
+//  AirContacts
+//
+//  Created by Piotr Waś on 28/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct AirContactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
